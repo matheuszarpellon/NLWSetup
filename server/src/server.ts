36136cddc4f7d33e2c99c77ff54642f44929f928
Matheus Zarpellon <mvzarpellon@gmail.com>
2023-01-18 00:44:16 -0300

    Aula 02 - backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,20 +1,16 @@
-import Fastify from 'fastify'
-import cors from '@fastify/cors'
-import { PrismaClient } from '@prisma/client'
+import cors from "@fastify/cors";
+import Fastify from "fastify";
+import { appRoutes } from "./routes";
 
-const app = Fastify()
-const prisma = new PrismaClient()
+const app = Fastify();
 
-app.register(cors)
+app.register(cors);
+app.register(appRoutes);
 
-app.get('/hello', async () => {
-  const habits = await prisma.habit.findMany()
-
-  return habits
-})
-
-app.listen({
-  port: 3333,
-}).then(() => {
-  console.log('HTTP Server running!')
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("HTTP Server running!");
+  });
