bf97588a5780bb04e050960d2679016dc6835828
Matheus Zarpellon <mvzarpellon@gmail.com>
2023-01-19 00:26:01 -0300

    Aula 03 - frontend

---
+++ web/src/components/ProgressBar.tsx
@@ -0,0 +1,13 @@
+import * as Progress from '@radix-ui/react-progress'
+
+interface ProgressBarProps {
+  progress: number;
+}
+
+export function ProgressBar({ progress }: ProgressBarProps) {
+  return (
+    <Progress.Root className="h3 rounded-xl bg-zinc-700 w-full mt-4">
+      <Progress.Indicator className="h-3 rounded-xl bg-violet-600" style={{ width: `${progress}%` }} />
+    </Progress.Root>
+  )
+}
--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -35,7 +35,7 @@ export function SummaryTable() {
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDates.map((date) => {
           return (
-            <HabitDay key={date.toString()} />
+            <HabitDay key={date.toString()} amount={10} completed={Math.round(Math.random() * 10)} />
           )
         })}
 
